056daeb1e397916818854db3c96925d6340ad40c
kamzhanyue <kamzhanyue@gmail.com>
2024-08-19 00:45:10 +1000

    Implement climbing score tween

--- scripts/ui/score_popup.gd
+++ scripts/ui/score_popup.gd
@@ -3,7 +3,8 @@ extends Control
 @onready var score_label := %ScoreLabel as RichTextLabel
 @onready var combo_label := %ComboLabel as RichTextLabel
 
-var tween: Tween
+var score_tween: Tween
+var combo_tween: Tween
 
 func init(state: GameState) -> void:
 	_on_score(0.0)
@@ -12,8 +13,15 @@ func init(state: GameState) -> void:
 	state.on_combo.connect(_on_combo)
 
 func _on_score(value: float) -> void:
-	score_label.text = str(value)
-	pass
+	if score_tween:
+		score_tween.kill()
+	var current_score := float(score_label.text)
+	score_tween = get_tree().create_tween()
+	score_tween.set_trans(Tween.TRANS_LINEAR)
+	score_tween.tween_method(set_score_label, current_score, value, 1.0)
+
+func set_score_label(value: float) -> void:
+	score_label.text = str(int(value))
 
 func _on_combo(value: int) -> void:
 	if value == 0:
@@ -23,10 +31,10 @@ func _on_combo(value: int) -> void:
 	var combo := str("Combo: ", value, "x")
 	var shake := str("[shake rate=",value," level=10 connected=1]", combo, "[/shake]")
 	combo_label.text = shake
-	if tween:
-		tween.kill()
+	if combo_tween:
+		combo_tween.kill()
 	combo_label.scale = Vector2.ONE
-	tween = get_tree().create_tween()
-	tween.tween_property(combo_label, "scale", Vector2(1.25, 1.25), 0.08).set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(combo_label, "scale", Vector2.ONE, 0.08).set_trans(Tween.TRANS_CUBIC)
-	tween.set_ease(Tween.EASE_OUT)
+	combo_tween = get_tree().create_tween()
+	combo_tween.tween_property(combo_label, "scale", Vector2(1.25, 1.25), 0.08).set_trans(Tween.TRANS_CUBIC)
+	combo_tween.tween_property(combo_label, "scale", Vector2.ONE, 0.08).set_trans(Tween.TRANS_CUBIC)
+	combo_tween.set_ease(Tween.EASE_OUT)
