8720121426dad13f59a5bbd23a33eb936cb9f226
kamzhanyue <kamzhanyue@gmail.com>
2024-08-18 23:57:09 +1000

    Implement combo animation

--- scenes/ui/score_popup.gd
+++ scenes/ui/score_popup.gd
@@ -3,6 +3,8 @@ extends Control
 @onready var score_label := %ScoreLabel as RichTextLabel
 @onready var combo_label := %ComboLabel as RichTextLabel
 
+var tween: Tween
+
 func init(state: GameState) -> void:
 	_on_score(0.0)
 	_on_combo(0)
@@ -16,8 +18,15 @@ func _on_score(value: float) -> void:
 func _on_combo(value: int) -> void:
 	if value == 0:
 		Global.set_inactive(combo_label)
-	else:
-		Global.set_active(combo_label)
-		var combo = str("Combo: ", value, "x")
-		var shake = str("[shake rate=",value," level=10 connected=1]", combo, "[/shake]")
-		combo_label.text = shake
+		return
+	Global.set_active(combo_label)
+	var combo := str("Combo: ", value, "x")
+	var shake := str("[shake rate=",value," level=10 connected=1]", combo, "[/shake]")
+	combo_label.text = shake
+	if tween:
+		tween.kill()
+	combo_label.scale = Vector2.ONE
+	tween = get_tree().create_tween()
+	tween.tween_property(combo_label, "scale", Vector2(1.25, 1.25), 0.08).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(combo_label, "scale", Vector2.ONE, 0.08).set_trans(Tween.TRANS_CUBIC)
+	tween.set_ease(Tween.EASE_OUT)
