b141bd3074c1a33847b4a5fa84a63301f2ef6682
kamzhanyue <kamzhanyue@gmail.com>
2024-08-19 13:08:55 +1000

    Polish shark behaviour

--- scripts/entities/satellite.gd
+++ scripts/entities/satellite.gd
@@ -1,20 +1,49 @@
 class_name Satellite
 extends Enemy
 
-@export var orbit_radius := 500.0
+@export var orbit_radius := 100.0
 @export var orbit_period := 10.0
 @export var speed := 200.0
 @export var laser_damage := 5.0
 
 const LASER = preload("res://scenes/projectiles/laser.tscn")
+enum State { MOVING, ORBITING }
+var state := State.MOVING
+var angle := 0.0
 
-func _ready() -> void:
-	on_init.connect(_on_init)
-	
-func _on_init() -> void:
-	await lerp_async()
-	orbit_async()
+func _process(delta: float) -> void:
+	if not game_state:
+		return
+	if state == State.MOVING:
+		move()
+	elif state == State.ORBITING:
+		orbit()
+
+func move() -> void:
+	var start_pos := global_position
+	var player_pos := game_state.player.global_position
+	var difference := player_pos - start_pos
+	var direction := difference.normalized()
+	if difference.length() <= orbit_radius:
+		start_orbit()
+		return
+	global_position += direction * speed * get_process_delta_time()
+	pass
+
+func start_orbit() -> void:
+	state = State.ORBITING
 	shoot_async()
+	var player_pos := game_state.player.global_position
+	var difference := global_position - player_pos
+	angle = atan2(difference.y, difference.x)
+	print('angle is: ', rad_to_deg(angle))
+	
+func orbit() -> void:
+	var player_pos := game_state.player.global_position
+	var x := player_pos.x + cos(angle) * orbit_radius
+	var y := player_pos.y - cos(angle) * orbit_radius
+	global_position = Vector2(x, y)
+	pass
 
 func lerp_async() -> void:
 	var start_pos := position
--- scripts/entities/shark.gd
+++ scripts/entities/shark.gd
@@ -3,6 +3,7 @@ extends Enemy
 
 @export var bite_range := 50.0
 @export var speed := 75.0
+@export var retreat_speed := 25.0
 @export var bite_time := 2.0
 @export var bite_damage := 10.0
 @onready var audio := $AudioStreamPlayer2D as AudioStreamPlayer2D
@@ -36,12 +37,12 @@ func move_to_player() -> void:
 	var player_pos := game_state.player.global_position
 	var difference := player_pos - start_pos
 	var direction := difference.normalized()
-	if difference.length() <= bite_range:
-		return
-	global_position += direction * speed * get_process_delta_time()
 	var angle := atan2(direction.y, direction.x)
 	rotation = angle
 	sprite.flip_v = Global.flip_v(angle)
+	if difference.length() <= bite_range:
+		return
+	global_position += direction * speed * get_process_delta_time()
 
 func bite_async() -> void:
 	state = State.BITING
@@ -61,7 +62,7 @@ func retreat() -> void:
 	var player_pos := game_state.player.global_position
 	var difference := start_pos - player_pos
 	var direction := difference.normalized()
-	global_position += direction * speed * get_process_delta_time()
-	var angle := atan2(direction.y, direction.x)
-	rotation = angle
-	sprite.flip_v = Global.flip_v(angle)
+	global_position += direction * retreat_speed * get_process_delta_time()
+	#var angle := atan2(direction.y, direction.x)
+	#rotation = angle
+	#sprite.flip_v = Global.flip_v(angle)
